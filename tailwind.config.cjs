98a2d83ba4b8fd576924e055a9bf2daef39470cb
Thomas Dumond <a1224158@adelaide.edu.au>
2025-12-02 21:03:14 +1030

    netlify missing files

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./layouts/**/*.{html,js}",
+    "./content/**/*.{html,md,Rmd,ipynb}",
+    "./themes/**/layouts/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
